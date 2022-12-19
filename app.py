e94dcd1b27a6daac0332bfa60992939c22b0ff63
Moaksh <moakshkakar@gmail.com>
2022-12-20 01:00:46 +0530

    Application Update

--- app.py
+++ app.py
@@ -1,43 +1,67 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDRectangleFlatButton,MDRaisedButton
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivymd.uix.label import MDLabel
-Window.size = (400, 700)
-KV = """  
-MDLabel:
-    font_name: 'Fonts/Montserrat-ExtraBold.ttf'
-"""
+from kivymd.uix.filemanager import MDFileManager
+import os
 
-# creating Demo Class(base class)
+Window.size = (400, 500)
+
+
+
+kv = """
+
+BoxLayout:
+    orientation: 'vertical'
+    
+    MDTopAppBar:
+        title:"Brain Tumor Detetction"
+        
+    MDFloatLayout:
+        MDLabel:
+            text:"Bennett University has been set up at Greater Noida and commenced operations on 08 Aug 2016, initially with B.Tech and MBA programs. With a focus on giving students a premium learning experience, in an immersive environment at a campus designed by the internationally renowned RSP Architects, coupled with enhanced use of technology to meet students aspirations."
+            size_hint_x:(0.9)
+            font_style:"Body1"
+            pos_hint:{"center_x": 0.5, "center_y": 0.5}
+            
+        MDRaisedButton:
+            text:"Detect"
+            on_release:app.file_manager_open()
+            pos_hint:{"center_x": 0.5, "center_y": 0.2}
+
+"""
 class App(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.bind(on_keyboard=self.events)
+        self.manager_open = False
+        self.file_manager = MDFileManager(
+            exit_manager=self.exit_manager, select_path=self.select_path
+        )
+    def file_manager_open(self):
+        self.file_manager.show(os.path.expanduser("~"))  # output manager to the screen
+        self.manager_open = True
+
+    def select_path(self, path: str):
+        self.exit_manager()
+        print(path)
+
+    def exit_manager(self, *args):
+        self.manager_open = False
+        self.file_manager.close()
+
+    def events(self, instance, keyboard, keycode, text, modifiers):
+        if keyboard in (1001, 27):
+            if self.manager_open:
+                self.file_manager.back()
+        return True
 
     def build(self):
-        # screen = Builder.load_string(KV)
         screen = Screen()
-        txt = MDLabel(text = "Brain Tumor Detection", theme_text_color = "Primary", font_style = "H4", pos_hint={
-            'center_x': 0.58, 'center_y': 0.95})
-        screen.add_widget(txt)
-        # adding theme_color
         self.theme_cls.theme_style = "Dark"
-
-        txt = MDLabel(text="Bennett University has been set up at Greater Noida and commenced operations on 08 Aug 2016, initially with B.Tech and MBA programs. With a focus on giving students a premium learning experience, in an immersive environment at a campus designed by the internationally renowned RSP Architects, coupled with enhanced use of technology to meet students aspirations.", theme_text_color="Primary", font_style="Button")
-        screen.add_widget(txt)
-        btn = MDRaisedButton(text="Detect", pos_hint={
-            'center_x': 0.5, 'center_y': 0.8},
-                                    on_release=self.btnfunc1)
-        screen.add_widget(btn)
-
-        # returning the screen
-        return screen
-
-    def btnfunc(self, obj):
-        print("button is pressed!!")
-
-    def btnfunc1(self, obj):
-        print("Preessed!!")
+        return Builder.load_string(kv)
 
 
 if __name__ == "__main__":
     App().run()
+
